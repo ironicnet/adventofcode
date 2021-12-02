5ce8159fedd458ff57c3c23d6522827c9d5a4bb9
root <root@a6c0358b-b64f-42dd-adb8-eed99d5cfa5c.e3n3aos4upmupbviolj31rhxbf.bx.internal.cloudapp.net>
2021-12-02 13:35:18 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+
+const executeDefault = (command, file) => {
+    const filePath = `./${file}/${command}.js`;
+
+    const commandFn = require(filePath);
+
+    if (commandFn) {
+        console.error(`Executing ${filePath}`)
+        commandFn();
+    } else {
+        console.error(`No file found at ${filePath}`)
+    }
+}
+const commands = {
+    'run': 
+}
 var myArgs = process.argv.slice(2);
 
-const [ command, file ] = process.argv.slice(2);
+const [ command ] = process.argv.slice(1);
 
 if (!command) {
     console.info("No command found. Try `run` or `test`");
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
     return;
 }
 if (!file) {
     console.info("No path found");
     return;
-}
-const filePath = `./${file}/${command}.js`;
-
-const commandFn = require(filePath);
-
-if (commandFn) {
-    console.error(`Executing ${command} at ${filePath}`)
-    commandFn();
-} else {
-    console.error(`No command found at ${filePath}`)
 }
